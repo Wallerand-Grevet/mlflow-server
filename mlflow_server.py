bc9e0c79d82f0b6b2cbd01260e31f9e67fc7ddce
Wawa <wawa@Mac-mini-de-Wawa.local>
2025-03-22 15:47:14 +0100

    feat: connect MLflow to PostgreSQL on Render

--- mlflow_server.py
+++ mlflow_server.py
@@ -1,16 +1,25 @@
 import os
 import subprocess
 
-# Chemins dans /tmp pour éviter les problèmes de permissions
-ARTIFACT_PATH = "/tmp/mlruns"
-DB_PATH = "/tmp/mlflow.db"
+# Infos Render PostgreSQL
+PG_USER = "mlflow_db_hisu_user"
+PG_PASSWORD = "AasxbT5V64Q68UZO1uf4e0dpQW6noX5S"
+PG_HOST = "dpg-cvfco68fnakc739n1b3g-a"
+PG_PORT = "5432"
+PG_DATABASE = "mlflow-db"
 
-os.makedirs(ARTIFACT_PATH, exist_ok=True)
+# 👉 URI pour SQLAlchemy (backend store MLflow)
+backend_store_uri = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}"
 
+# Artifacts dans un dossier temporaire
+artifact_path = "/tmp/mlruns"
+os.makedirs(artifact_path, exist_ok=True)
+
+# Lancement du serveur MLflow
 subprocess.run([
     "mlflow", "server",
-    "--backend-store-uri", f"sqlite:///{DB_PATH}",
-    "--default-artifact-root", f"file:{ARTIFACT_PATH}",
+    "--backend-store-uri", backend_store_uri,
+    "--default-artifact-root", f"file:{artifact_path}",
     "--host", "0.0.0.0",
     "--port", "10000",
     "--serve-artifacts"
